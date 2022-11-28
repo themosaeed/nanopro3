2015be3f9b1c4107125581b3eef288785aaa5ee6
themosaeed <mo.elsaeed.aly@gmail.com>
2022-11-28 14:01:47 +0200

    migration8

--- .circleci/ansible/roles/configure-server/files/myenv.sh
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -1,9 +1,9 @@
 export NODE_ENV=production
 export VERSION=1
 export TYPEORM_CONNECTION=postgres
-export TYPEORM_HOST=udapeople-db.caounmzo6jzy.us-west-2.rds.amazonaws.com
+export TYPEORM_HOST=database.caounmzo6jzy.us-west-2.rds.amazonaws.com
 export TYPEORM_USERNAME=postgres
-export TYPEORM_PASSWORD=NbBQWRVYUexvuzbiVzUj
+export TYPEORM_PASSWORD=DU1g7Y3PmV4VuebhhhNU
 export TYPEORM_DATABASE=glee
 export TYPEORM_PORT=5432
 export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity{.ts,.js}
